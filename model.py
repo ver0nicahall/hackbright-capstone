921e7ecf6a0564e8d999f0924f81bf43c471f7b4
Veronica Hall <86692664+ver0nicahall@users.noreply.github.com>
2022-05-04 15:12:20 -0500

    add route for homepage

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 """Models for clothing rental app."""
 
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -31,4 +32,24 @@ class Items(db.Model):
 
     def __repr__(self):
         return f'<User user_id={self.user_id} email={self.email}>'
+
+
+def connect_to_db(flask_app, db_uri="postgresql:///ratings", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
+
+if __name__ == "__main__":
+    from server import app
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    connect_to_db(app)
 
--- server.py
+++ server.py
@@ -9,3 +9,9 @@ from jinja2 import StrictUndefined
 app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
+
+@app.route('/')
+def homepage():
+    """View homepage."""
+
+    return render_template("homepage.html")
