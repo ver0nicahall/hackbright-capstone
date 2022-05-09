d16107999a6d1e0da68d2ca2769216306b6b3c4e
Veronica Hall <86692664+ver0nicahall@users.noreply.github.com>
2022-05-09 02:03:10 -0500

    seed database with 50 items

--- seed_database.py
+++ seed_database.py
@@ -16,8 +16,13 @@ os.system("createdb rentals")
 model.connect_to_db(server.app)
 model.db.create_all()
 
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+adjs = ['fancy', 'cute', 'comfy', 'expensive', 'well-loved', 'gala']
+articles = ['hat', 'dress', 'shirt', 'jeans', 'skirt', 'overalls', 'onesie', 'costume']
+
+
 #create users and generate 10 fake items for each user
-for n in range(1, 10):
+for n in range(1, 11):
     email=f'user{n}@test.com'
     password='test{n}'
 
@@ -26,10 +31,18 @@ for n in range(1, 10):
     model.db.session.add(user)
 
     #for each user, create 5 items?
+    for n in range (1, 6):
+
+        #create a random name
+        name = (choice(colors) + ' ' + choice(adjs) + ' ' + choice(articles))
+
+        item = crud.create_item(name, 'test description', randint(1, 20), 0, 0, '123 main st', 'sunnyvale', 'CA', '94085', True, user)
+        #add item to database
+        model.db.session.add(item)
+
+
 model.db.session.commit()
 
 #create some fake rentals
-    
-#commit everything
-model.db.session.commit()
+
 
