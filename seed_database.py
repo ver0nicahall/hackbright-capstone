c30f4f550867e67a19099868b566ccf400e0af1c
Veronica Hall <86692664+ver0nicahall@users.noreply.github.com>
2022-05-09 00:07:56 -0500

    Seed database with 10 users

--- seed_database.py
+++ seed_database.py
@@ -15,3 +15,21 @@ os.system("createdb rentals")
 #create tables
 model.connect_to_db(server.app)
 model.db.create_all()
+
+#create users and generate 10 fake items for each user
+for n in range(1, 10):
+    email=f'user{n}@test.com'
+    password='test{n}'
+
+    #create a user, add to db 
+    user = crud.create_user(email, password)
+    model.db.session.add(user)
+
+    #for each user, create 5 items?
+model.db.session.commit()
+
+#create some fake rentals
+    
+#commit everything
+model.db.session.commit()
+
