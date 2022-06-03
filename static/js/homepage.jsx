e8237c749024e6a26fc1a628f5cf492e28c6006a
Veronica Hall <86692664+ver0nicahall@users.noreply.github.com>
2022-06-03 14:57:43 -0500

    attempt to add background image

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -2,8 +2,10 @@ function Homepage() {
     return (
       <div id="homepage-container">
           <h1> Welcome to Cloop!</h1>
+          <div id="process-entry">
               <div><a href="/signup">Sign Up</a></div>
               <div>Already have an account? <a href="/login">Log In</a></div>
+          </div>
 
       </div>
     );
