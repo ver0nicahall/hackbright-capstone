fbc5ad092d5e8b996409c1639a6bfd3527c2867a
Veronica Hall <86692664+ver0nicahall@users.noreply.github.com>
2022-06-06 23:15:00 -0500

    Finish styling for homepage

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,13 +1,16 @@
 function Homepage() {
     return (
       <div id="homepage-container">
-          <h1>Cloop</h1>
+          <div className="logo">CLOOP</div>
+          <p id="subline">(<span className="emphasize">Cl </span>ose the L<span className="emphasize"> oop </span>)</p>
           <div className="container">
             <div id="process-entry">
-                <div><a href="/signup">Sign Up</a></div>
+                <div id="byline">The premiere rental marketplace for clothing and accessories.</div>
+                <div id="signup-line"><a href="/signup">Sign Up</a>
+                </div>
                 <div>Already have an account? <a href="/login">Log In</a></div>
             </div>
-          </div>
+          </div>s
       </div>
     );
   }
