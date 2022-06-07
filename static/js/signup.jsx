7eca040a4471a69eccb55656bc98c620fabd33cf
Veronica Hall <86692664+ver0nicahall@users.noreply.github.com>
2022-06-06 23:27:45 -0500

    Finish styling signup page

--- static/js/signup.jsx
+++ static/js/signup.jsx
@@ -6,7 +6,7 @@ function Signup() {
               <div id="email-label" className="signup-form">Email <input type="text" name="email"/> </div> 
               <div id="password-label" className="signup-form">Password <input type="password" name="password"/></div>
               <div id="signup-submit" className="login-form"><input type="submit" /></div>
-              <div id="already" className="login-form">Already have an account? <a href="/login">Login!</a></div>
+              <div id="already" className="login-form">Already have an account? <a href="/login">Login</a></div>
           </form>
       </div>
     );
